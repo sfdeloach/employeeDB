adbc2285349937cd264455f616892565c16df33f
Steven DeLoach <sfdeloach@gmail.com>
2020-02-23 16:43:50 -0500

    commit on 'master'

--- src/main.cpp
+++ src/main.cpp
@@ -8,38 +8,38 @@ using namespace std;
 
 int main()
 {
-   ofstream outFile("../db/store", ios_base::binary);
+   ofstream outFile("./db/store", ios_base::binary);
    Name steven("Steven", "DeLoach");
    steven.write_binary(outFile);
    outFile.close();
 
-   ifstream inFile("../db/store", ios_base::binary);
+   ifstream inFile("./db/store", ios_base::binary);
    Name unknown;
    cout << unknown.getLastFirst() << endl;
    unknown.read_binary(inFile);
    cout << unknown.getLastFirst() << endl;
    inFile.close();
 
-   Date today(2020, 2, 20);
+   // Date today(2020, 2, 20);
 
-   ofstream outFile2;
-   outFile2.open("../db/store", ios_base::binary);
-   today.write_binary(outFile);
-   outFile2.close();
+   // ofstream outFile2;
+   // outFile2.open("../db/store", ios_base::binary);
+   // today.write_binary(outFile);
+   // outFile2.close();
 
-   ofstream outJSON2;
-   outJSON2.open("../db/store.json");
-   today.write_JSON(outJSON2);
-   outJSON2.close();
+   // ofstream outJSON2;
+   // outJSON2.open("../db/store.json");
+   // today.write_JSON(outJSON2);
+   // outJSON2.close();
 
-   Date unknownDate;
+   // Date unknownDate;
 
-   ifstream inFile2;
-   inFile2.open("../db/store", ios_base::binary);
-   unknownDate.read_binary(inFile);
-   inFile2.close();
+   // ifstream inFile2;
+   // inFile2.open("../db/store", ios_base::binary);
+   // unknownDate.read_binary(inFile);
+   // inFile2.close();
 
-   cout << unknownDate.toISODate() << endl;
+   // cout << unknownDate.toISODate() << endl;
 
    return 0;
 }
